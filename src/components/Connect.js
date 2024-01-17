64ffaebe153e43e90025b3837eacd975427822ce
Aishwarya-ukirde <114334826+Aishwarya-ukirde@users.noreply.github.com>
2024-01-17 10:41:42 +0530

    Update Connect.js

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -13,68 +13,71 @@ const Connect = () => {
   };
 
   return (
-    <div style={back} className="mt-[100px] md:h-[700px] h-[10px]">
-      <div className="md:h-[400px] md:w-[70%] lg:w-[50%] w-[90%] shadow-lg shadow-stone-400 rounded-lg self-center mt-10 md:p-10 p-2">
-        <div className="flex flex-col md:p-4 gap-4">
-          <div>
-            <label htmlFor="email" className="block text-gray-800 text-sm font-bold mb-2">
-              Email:
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Enter your email"
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-            />
-          </div>
-          <div>
-            <label htmlFor="phone" className="block text-gray-800 text-sm font-bold mb-2">
-              Phone:
-            </label>
-            <input
-              type="tel"
-              id="phone"
-              name="phone"
-              placeholder="Enter your phone number"
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-            />
-          </div>
-          <div>
-            <label htmlFor="name" className="block text-gray-800 text-sm font-bold mb-2">
-              Name:
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Enter your name"
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-            />
-          </div>
+    <div style={back} className='mt-[100px] h-[10px] md:h-[700px]'>
+      <div className="md:h-[400px]  md:w-[700px]  w-[500px]  shadow-lg shadow-stone-400 rounded-lg self-center mt-10 md:p-10 p-2 ">
+       <div className='flex flex-row gap-2 justify-between md:p-4  '>
+        <div className='basic-1/2'>
+        <form className="flex flex-col gap-4">
+            <div>
+              <label htmlFor="email" className="block text-gray-800 text-sm font-bold mb-2">
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter your email"
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+              />
         </div>
+</form>  <div>
+              <label htmlFor="phone" className="block text-gray-800 text-sm font-bold mb-2">
+                Phone:
+              </label>
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                placeholder="Enter your phone number"
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+              />
+            </div>
+            <div>
+              <label htmlFor="name" className="block text-gray-800 text-sm font-bold mb-2">
+                Name:
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Enter your name"
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+              />
+            </div>
+            </div>
 
-        <div>
-          <label htmlFor="message" className="block text-gray-800 text-sm font-bold mb-2">
-            Message:
-          </label>
-          <textarea
-            id="message"
-            name="message"
-            placeholder="Type your message"
-            rows="4"
-            className="w-full px-4 py-2 border rounded-md resize-none focus:outline-none focus:border-blue-500"
-          ></textarea>
-        </div>
-
-        <div className="flex items-center justify-center mt-4">
-          <button
-            type="submit"
-            className="bg-blue-500 text-white py-2 w-[250px] rounded-full hover:shadow-md hover:shadow-blue-400"
-          >
-            Get started
-          </button>
-        </div>
+        <div><div>
+            <label htmlFor="message" className="block text-gray-800 text-sm font-bold mb-2">
+              Message:
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Type your message"
+              rows="4"
+              className="w-full px-4 py-2 border rounded-md resize-none focus:outline-none focus:border-blue-500"
+            ></textarea>
+          </div></div>
+       </div>
+       <div><div className="flex items-center justify-center mt-4">
+        <button
+          type="submit"
+          className="bg-blue-500 text-white py-2 w-[250px] rounded-full  hover:shadow-md  hover:shadow-blue-400"
+        >
+          Get started
+        </button>
+      </div>
+       </div>
       </div>
     </div>
   );
